768b4a082022960fe49de50c71a28e7f06f0854f
Marek Kirejczyk <marekkirejczyk@users.noreply.github.com>
2019-01-15 15:53:20 +0100

    Allow .js  config file (#56)
    * allow js as a config file

---
+++ test/projects/custom/config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  name: "solidity 5, solcjs",
+  sourcesPath: "./test/projects/custom/custom_contracts",
+  targetPath: "./test/projects/custom/custom_build",
+  npmPath: "./test/projects/custom/custom_node_modules"
+};
