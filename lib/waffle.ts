65d92e20c289be4ba11c8fab7bc68fa529cba441
Cezary Haliniarz <35804520+cehali@users.noreply.github.com>
2019-08-23 18:54:26 +0200

    Add path to config parameter in createMockProvider (#133)

--- lib/waffle.ts
+++ lib/waffle.ts
@@ -8,10 +8,19 @@ import './matchers/matchertypes';
 
 const defaultGanacheOptions = {accounts: defaultAccounts};
 
-export function createMockProvider(ganacheOptions: GanacheOpts = {}) {
+export const createMockProvider = (ganacheOptionsOrPathToConfig: string | GanacheOpts = {}) => {
+  const ganacheOptions = getGanacheOptions(ganacheOptionsOrPathToConfig);
   const options = {...defaultGanacheOptions, ...ganacheOptions };
   return new providers.Web3Provider(Ganache.provider(options));
-}
+};
+
+export const getGanacheOptions = (ganacheOptionsOrPathToConfig: string | GanacheOpts) => {
+  if (typeof ganacheOptionsOrPathToConfig === 'object') {
+    return ganacheOptionsOrPathToConfig;
+  }
+  const {ganacheOptions} = require(ganacheOptionsOrPathToConfig);
+  return ganacheOptions;
+};
 
 export function getWallets(provider: providers.Provider) {
   return defaultAccounts.map((account) => new Wallet(account.secretKey, provider));
