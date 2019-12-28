ac976d07a3b96b9d2ace104be13bba8be9b4e422
Piotr Szlachciak <szlachciak.piotr@gmail.com>
2019-12-28 19:26:06 +0100

    Rename @waffle/mock-provider to @waffle/provider

--- waffle-provider/src/deprecate.ts
+++ waffle-provider/src/deprecate.ts
@@ -1,5 +1,5 @@
 export function deprecate (what: string, message?: string) {
   if (process.env.NODE_ENV !== 'production') {
-    console.warn(`Deprecation warning: ${what} is deprecated.`, message)
+    console.warn(`Deprecation warning: ${what} is deprecated.${message ? ' ' + message : ''}`)
   }
 }
