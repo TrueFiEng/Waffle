5c273e61f3e3fa1db035f6f802d0c8d9966b0961
Piotr Szlachciak <szlachciak.piotr@gmail.com>
2020-02-27 11:55:11 +0100

    Add config value checking

---
+++ waffle-compiler/src/config/defaultConfig.ts
@@ -0,0 +1,14 @@
+import {Config} from './config';
+
+export const defaultConfig: Config = {
+  sourceDirectory: './contracts',
+  outputDirectory: './build',
+  nodeModulesDirectory: './node_modules',
+  compilerType: 'solcjs',
+  compilerVersion: 'default',
+  compilerAllowedPaths: [],
+  compilerOptions: {},
+  outputHumanReadableAbi: false,
+  outputMinimal: false,
+  outputType: 'multiple'
+};
