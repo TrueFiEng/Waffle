0578492f4dab2b804b32bc26f343a6bb096d3965
Piotr Szlachciak <szlachciak.piotr@gmail.com>
2020-03-11 14:12:39 +0100

    Simplify e2e compiler tests

---
+++ waffle-compiler/test/projects/solidity5/config_commonjs.js
@@ -0,0 +1,7 @@
+module.exports = {
+  name: 'Solidity 5 - commonjs',
+  sourceDirectory: './custom_contracts',
+  outputDirectory: './custom_build',
+  nodeModulesDirectory: './custom_node_modules',
+  compilerVersion: 'v0.5.9+commit.e560f70d',
+}
---
+++ waffle-compiler/test/projects/solidity5/config_promise.js
@@ -0,0 +1,7 @@
+module.exports = Promise.resolve({
+  name: 'Solidity 5 - promise',
+  sourceDirectory: './custom_contracts',
+  outputDirectory: './custom_build',
+  nodeModulesDirectory: './custom_node_modules',
+  compilerVersion: 'v0.5.9+commit.e560f70d',
+})
