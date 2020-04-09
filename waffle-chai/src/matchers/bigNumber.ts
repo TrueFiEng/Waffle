b8abae8fbef0f55d30c17e46fa63b7aef5c11381
Piotr Szlachciak <szlachciak.piotr@gmail.com>
2020-04-09 11:19:59 +0200

    Fix comparison bug for bn matchers

--- waffle-chai/src/matchers/bigNumber.ts
+++ waffle-chai/src/matchers/bigNumber.ts
@@ -3,18 +3,28 @@ import {utils} from 'ethers';
 export function supportBigNumber(Assertion: Chai.AssertionStatic, utils: Chai.ChaiUtils) {
   Assertion.overwriteMethod('equal', override('eq', 'equal', utils));
   Assertion.overwriteMethod('eq', override('eq', 'equal', utils));
+
   Assertion.overwriteMethod('above', override('gt', 'above', utils));
+  Assertion.overwriteMethod('gt', override('gt', 'greater than', utils));
+
   Assertion.overwriteMethod('below', override('lt', 'below', utils));
+  Assertion.overwriteMethod('lt', override('lt', 'less than', utils));
+
   Assertion.overwriteMethod('least', override('gte', 'at least', utils));
+  Assertion.overwriteMethod('gte', override('gte', 'greater than or equal', utils));
+
   Assertion.overwriteMethod('most', override('lte', 'at most', utils));
+  Assertion.overwriteMethod('lte', override('lte', 'less than or equal', utils));
 }
 
-function override(method: keyof utils.BigNumber, name: string, utils: Chai.ChaiUtils) {
+type Methods = 'eq' | 'gt' | 'lt' | 'gte' | 'lte';
+
+function override(method: Methods, name: string, utils: Chai.ChaiUtils) {
   return (_super: Function) => overwriteBigNumberFunction(method, name, _super, utils);
 }
 
 function overwriteBigNumberFunction(
-  functionName: keyof utils.BigNumber,
+  functionName: Methods,
   readableName: string,
   _super: Function,
   chaiUtils: Chai.ChaiUtils
@@ -22,17 +32,12 @@ function overwriteBigNumberFunction(
   return function (this: Chai.AssertionStatic, ...args: any[]) {
     const [actual] = args;
     const expected = chaiUtils.flag(this, 'object');
-    if (utils.BigNumber.isBigNumber(expected)) {
+    if (
+      utils.BigNumber.isBigNumber(expected) ||
+      utils.BigNumber.isBigNumber(actual)
+    ) {
       this.assert(
-        expected[functionName](actual),
-        `Expected "${expected}" to be ${readableName} ${actual}`,
-        `Expected "${expected}" NOT to be ${readableName} ${actual}`,
-        expected,
-        actual
-      );
-    } else if (utils.BigNumber.isBigNumber(actual)) {
-      this.assert(
-        actual[functionName](expected),
+        utils.bigNumberify(expected)[functionName](actual),
         `Expected "${expected}" to be ${readableName} ${actual}`,
         `Expected "${expected}" NOT to be ${readableName} ${actual}`,
         expected,
