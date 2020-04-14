a90e3c43e93cf0d3ce4baca79a2f318f11bd7d8e
Ivan Rukhavets <ivanruch@gmail.com>
2020-04-14 16:23:43 +0300

    fix build

--- waffle-doppelganger/src/index.ts
+++ waffle-doppelganger/src/index.ts
@@ -1,10 +1,14 @@
-import {utils, Contract, Wallet} from 'ethers';
+import {utils, Contract, Wallet, ContractFactory} from 'ethers';
 import {FunctionDescription} from 'ethers/utils/interface';
 import {EventFragment, FunctionFragment, ParamType} from 'ethers/utils/abi-coder';
-import {deployContract} from 'ethereum-waffle';
 import DoppelgangerContract from '../build/Doppelganger.json';
 
 type ABI = Array<EventFragment | FunctionFragment | ParamType> | string;
+
+async function deployDoppelganger(wallet: Wallet) {
+  const factory = new ContractFactory(DoppelgangerContract.abi, DoppelgangerContract.bytecode, wallet);
+  return factory.deploy();
+}
 
 function stub(mockContract: Contract, encoder: utils.AbiCoder, func: FunctionDescription) {
   return {
@@ -24,7 +28,7 @@ export type Doppelganger = {
 
 export async function doppelganger(wallet: Wallet, abi: ABI, contractInstance?: Contract):
 Promise<Doppelganger> {
-  const instance = contractInstance ?? await deployContract(wallet, DoppelgangerContract);
+  const instance = contractInstance ?? await deployDoppelganger(wallet);
 
   const {functions} = new utils.Interface(abi);
   const encoder = new utils.AbiCoder();
