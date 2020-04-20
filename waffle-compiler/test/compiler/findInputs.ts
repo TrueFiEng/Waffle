836827c505c2ff98fb9f0f9ad8617b813ae8ad46
VladStarostenko <38181661+VladStarostenko@users.noreply.github.com>
2020-04-20 14:14:43 +0300

    Fix tests

--- waffle-compiler/test/compiler/findInputs.ts
+++ waffle-compiler/test/compiler/findInputs.ts
@@ -3,14 +3,14 @@ import {expect} from 'chai';
 import {findInputs} from '../../src/findInputs';
 
 describe('INTEGRATION: findInputs', () => {
-  const extensions: string[] = ['.sol', '.vy'];
-
   before(async () => {
     await fs.mkdirp('/tmp/waffle/a');
     await fs.mkdirp('/tmp/waffle/b');
+    await fs.mkdirp('/tmp/waffle/d');
     await fs.mkdirp('/tmp/waffle/c/c');
 
     await fs.writeFile('/tmp/waffle/b/b.sol', '');
+    await fs.writeFile('/tmp/waffle/d/d.vy', '');
     await fs.writeFile('/tmp/waffle/b/xxx.xxx', '');
     await fs.writeFile('/tmp/waffle/c/c1.sol', '');
     await fs.writeFile('/tmp/waffle/c/c/c2.sol', '');
@@ -21,19 +21,26 @@ describe('INTEGRATION: findInputs', () => {
   });
 
   it('returns empty array for an empty forlder', () => {
-    const result = findInputs('/tmp/waffle/a', extensions);
+    const result = findInputs('/tmp/waffle/a', '.sol');
     expect(result).to.deep.equal([]);
   });
 
   it('only returns .sol files', () => {
-    const result = findInputs('/tmp/waffle/b', extensions);
+    const result = findInputs('/tmp/waffle/b', '.sol');
     expect(result).to.deep.equal([
       '/tmp/waffle/b/b.sol'
     ]);
   });
 
+  it('only returns .vy files', () => {
+    const result = findInputs('/tmp/waffle/d', '.vy');
+    expect(result).to.deep.equal([
+      '/tmp/waffle/d/d.vy'
+    ]);
+  });
+
   it('returns files found in a folder recursively', () => {
-    const result = findInputs('/tmp/waffle/c', extensions);
+    const result = findInputs('/tmp/waffle/c', '.sol');
     expect(result).to.deep.equal([
       '/tmp/waffle/c/c1.sol',
       '/tmp/waffle/c/c/c2.sol'
