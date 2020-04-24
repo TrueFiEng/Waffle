7de57c8f8ae11a2790ba9a3d06d5ecbc2a531ef3
jozwiak <kubencki@gmail.com>
2020-04-24 19:44:00 +0200

    fix linter

--- waffle-chai/test/matchers/ethCalled/calledOnContractWith.test.ts
+++ waffle-chai/test/matchers/ethCalled/calledOnContractWith.test.ts
@@ -41,7 +41,7 @@ describe('INTEGRATION: calledOnContractWith', () => {
 
     await contract.callWithParameter(2);
 
-    expect('callWithParameter').not.to.be.to.be.calledOnContractWith(contract, [1]);
+    expect('callWithParameter').not.to.be.calledOnContractWith(contract, [1]);
   });
 
   it('checks that contract function with parameters was not called', async () => {
@@ -49,7 +49,7 @@ describe('INTEGRATION: calledOnContractWith', () => {
 
     await contract.callWithParameters(1, 2);
 
-    expect('callWithParameters').not.to.be.to.be.calledOnContractWith(contract,[1, 3]);
+    expect('callWithParameters').not.to.be.calledOnContractWith(contract, [1, 3]);
   });
 
   it('throws assertion error when contract function with parameter was called', async () => {
@@ -57,7 +57,7 @@ describe('INTEGRATION: calledOnContractWith', () => {
     await contract.callWithParameter(2);
 
     expect(
-      () => expect('callWithParameter').not.to.be.calledOnContractWith(contract,[2])
+      () => expect('callWithParameter').not.to.be.calledOnContractWith(contract, [2])
     ).to.throw(AssertionError, 'Expected contract function with parameters NOT to be called');
   });
 });
