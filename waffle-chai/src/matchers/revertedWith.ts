d08a7e0b7f96a1cf7cbd2b859ae3b004650af0f9
Pedro Gomes <pedrogomes94@gmail.com>
2020-05-04 13:35:37 +0200

    catch reverts

--- waffle-chai/src/matchers/revertedWith.ts
+++ waffle-chai/src/matchers/revertedWith.ts
@@ -11,21 +11,20 @@ export function supportRevertedWith(Assertion: Chai.AssertionStatic) {
           'Transaction NOT reverted.'
         );
         return value;
-      },
-      (reason: any) => {
-        reason = (reason instanceof Object && 'message' in reason) ? reason.message : reason;
-        const isReverted = reason.toString().search('revert') >= 0 && reason.toString().search(revertReason) >= 0;
-        const isThrown = reason.toString().search('invalid opcode') >= 0 && revertReason === '';
-        this.assert(
-          isReverted || isThrown,
-          `Expected transaction to be reverted with ${revertReason}, but other exception was thrown: ${reason}`,
-          `Expected transaction NOT to be reverted with ${revertReason}`,
-          `Transaction reverted with ${revertReason}.`,
-          reason
-        );
-        return reason;
       }
-    );
+    ).catch((reason: any) => {
+      const message = (reason instanceof Object && 'message' in reason) ? reason.message : reason.toString();
+      const isReverted = message.search('revert') >= 0 && message.search(revertReason) >= 0;
+      const isThrown = message.search('invalid opcode') >= 0 && revertReason === '';
+      this.assert(
+        isReverted || isThrown,
+        `Expected transaction to be reverted with ${revertReason}, but other exception was thrown: ${reason}`,
+        `Expected transaction NOT to be reverted with ${revertReason}`,
+        `Transaction reverted with ${revertReason}.`,
+        reason
+      );
+      return reason;
+    });
     this.then = derivedPromise.then.bind(derivedPromise);
     this.catch = derivedPromise.catch.bind(derivedPromise);
     return this;
