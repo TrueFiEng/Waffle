19f3919871d8c4161af64b810ae32ef936b58533
Piotr Szlachciak <szlachciak.piotr@gmail.com>
2020-05-07 17:19:47 +0200

    Customize looks

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -46,3 +46,9 @@ exclude_patterns = []
 #
 html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
+html_css_files = ['custom.css']
+html_favicon = '_static/favicon.ico'
+html_logo = '_static/waffle-logo-square.svg'
+html_theme_options = {
+  'logo_only': True,
+}
