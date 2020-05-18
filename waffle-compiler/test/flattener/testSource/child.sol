b1dea513078663288762d981ef64095ce9a6fcb0
Ivan Rukhavets <ivanruch@gmail.com>
2020-05-18 15:59:33 +0300

    Fix problem with flat contract import order

--- waffle-compiler/test/flattener/testSource/child.sol
+++ waffle-compiler/test/flattener/testSource/child.sol
@@ -1,5 +1,6 @@
 pragma solidity >=0.4.24 <0.6.0;
 
+import "openzeppelin-solidity/contracts/access/Roles.sol";
 import "./parent.sol";
 import "openzeppelin-solidity/contracts/access/roles/PauserRole.sol";
 
