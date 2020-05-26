bdc22d710c0dcc2a9fa871fbf26940e17ca1f74a
Vlad Starostenko <vlad.starostenko.0501@gmail.com>
2020-05-26 12:53:33 +0300

    Fix example

--- examples/ens/test/ENS.test.ts
+++ examples/ens/test/ENS.test.ts
@@ -1,6 +1,5 @@
 import chai, {expect} from 'chai';
 import {utils} from 'ethers';
-import {ENS, deployENS} from '@ethereum-waffle/ens';
 import {MockProvider} from '@ethereum-waffle/provider';
 import chaiAsPromised from 'chai-as-promised';
 
@@ -11,43 +10,25 @@ const {namehash} = utils;
 describe('Deploy Ens', async () => {
   const provider = new MockProvider();
   const [wallet] = provider.getWallets();
-  let ens: ENS;
 
-  beforeEach(async () => {
-    ens = await deployENS(wallet);
-    await ens.createTopLevelDomain('test');
+  before(async () => {
+    await provider.setupENS();
   });
 
-  describe('Create domain', async () => {
-    it('sub domain', async () => {
-      const node = namehash('ethworks.test');
-      await ens.createSubDomain('ethworks.test');
-      expect(await ens.ens.owner(node)).to.eq(ens.registrars['ethworks.test'].address);
-      expect(await ens.ens.resolver(node)).to.eq(ens.resolver.address);
-    });
-
-    it('sub domain recursive', async () => {
-      const node = namehash('ethworks.tld');
-      await ens.createSubDomain('ethworks.tld', {recursive: true});
-      expect(await ens.ens.owner(node)).to.eq(ens.registrars['ethworks.tld'].address);
-      expect(await ens.ens.resolver(node)).to.eq(ens.resolver.address);
-    });
+  it('Create domain recursive', async () => {
+    const node = namehash('ethworks.tld');
+    await provider.ens.createSubDomain('ethworks.tld', {recursive: true});
+    expect(await provider.ens.ens.owner(node)).to.eq(provider.ens.registrars['ethworks.tld'].address);
+    expect(await provider.ens.ens.resolver(node)).to.eq(provider.ens.resolver.address);
   });
 
-  describe('Set address', async () => {
-    it('Recursive', async () => {
-      const node = namehash('vlad.test.tld');
-      await ens.setAddress('vlad.test.tld', ens.wallet.address, {recursive: true});
-      expect(await ens.ens.owner(node)).to.eq(ens.wallet.address);
-      expect(await ens.resolver['addr(bytes32)'](node)).to.eq(ens.wallet.address);
-      expect(await ens.ens.resolver(node)).to.eq(ens.resolver.address);
-    });
-    it('Reverse', async () => {
-      await ens.setAddressWithReverse('vlad.ethworks.test', wallet, {recursive: true});
-      const node = namehash(wallet.address.slice(2) + '.addr.reverse');
-      expect(await ens.ens.owner(node)).to.eq(ens.reverseRegistrar.address);
-      expect(await ens.ens.resolver(node)).to.eq(ens.resolver.address);
-      expect(await ens.resolver.name(node)).to.eq('vlad.ethworks.test');
-    });
+  it('Set address', async () => {
+    await provider.ens.setAddress('vlad.ethworks.tld', wallet.address);
+    expect(await provider.resolveName('vlad.ethworks.tld')).to.eq(wallet.address);
+  });
+
+  it('Set address with reverse ', async () => {
+    await provider.ens.setAddressWithReverse('vlad.ethworks.test', wallet, {recursive: true});
+    expect(await provider.lookupAddress(wallet.address)).to.eq('vlad.ethworks.test');
   });
 });
