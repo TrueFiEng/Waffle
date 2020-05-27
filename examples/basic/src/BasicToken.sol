9c64f523cb82fe7366736316518ca9ad1bba8b41
VladStarostenko <38181661+VladStarostenko@users.noreply.github.com>
2020-05-27 15:59:09 +0300

    Add basic example (#286)
    * Add basic example

---
+++ examples/basic/src/BasicToken.sol
@@ -0,0 +1,10 @@
+pragma solidity ^0.5.1;
+
+import "openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
+
+// Example class - a mock class using delivering from ERC20
+contract BasicToken is ERC20 {
+  constructor(address initialAccount, uint256 initialBalance) public {
+    super._mint(initialAccount, initialBalance);
+  }
+}
---
+++ examples/basic/test/BasicToken.test.ts
@@ -0,0 +1,50 @@
+import {expect, use} from 'chai';
+import {Contract} from 'ethers';
+import {deployContract, MockProvider, solidity} from 'ethereum-waffle';
+import BasicToken from '../build/BasicToken.json';
+
+use(solidity);
+
+describe('BasicToken', () => {
+  const [wallet, walletTo] = new MockProvider().getWallets();
+  let token: Contract;
+
+  beforeEach(async () => {
+    token = await deployContract(wallet, BasicToken, [wallet.address, 1000]);
+  });
+
+  it('Assigns initial balance', async () => {
+    expect(await token.balanceOf(wallet.address)).to.equal(1000);
+  });
+
+  it('Transfer adds amount to destination account', async () => {
+    await token.transfer(walletTo.address, 7);
+    expect(await token.balanceOf(walletTo.address)).to.equal(7);
+  });
+
+  it('Transfer emits event', async () => {
+    await expect(token.transfer(walletTo.address, 7))
+      .to.emit(token, 'Transfer')
+      .withArgs(wallet.address, walletTo.address, 7);
+  });
+
+  it('Can not transfer above the amount', async () => {
+    await expect(token.transfer(walletTo.address, 1007)).to.be.reverted;
+  });
+
+  it('Can not transfer from empty account', async () => {
+    const tokenFromOtherWallet = token.connect(walletTo);
+    await expect(tokenFromOtherWallet.transfer(wallet.address, 1))
+      .to.be.reverted;
+  });
+
+  it('Calls totalSupply on BasicToken contract', async () => {
+    await token.totalSupply();
+    expect('totalSupply').to.be.calledOnContract(token);
+  });
+
+  it('Calls balanceOf with sender address on BasicToken contract', async () => {
+    await token.balanceOf(wallet.address);
+    expect('balanceOf').to.be.calledOnContractWith(token, [wallet.address]);
+  });
+});
