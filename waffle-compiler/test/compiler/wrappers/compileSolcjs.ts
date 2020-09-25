e2d85f6ee0b4862f6eec57590e8bc091cc066f1b
Piotr Szlachciak <szlachciak.piotr@gmail.com>
2020-09-25 16:16:15 +0200

    Skip solc download if version is already present

--- waffle-compiler/test/compiler/wrappers/compileSolcjs.ts
+++ waffle-compiler/test/compiler/wrappers/compileSolcjs.ts
@@ -11,6 +11,21 @@ describe('INTEGRATION: loadCompiler', () => {
     expect(solcLoaded.version()).to.equal('0.5.9+commit.e560f70d.Emscripten.clang');
   });
 
+  it('loadCompiler with a semver version as version', async () => {
+    const solcLoaded = await loadCompiler(
+      {compilerVersion: '0.7.0'} as Config
+    );
+    expect(solcLoaded.version()).to.startWith('0.7.0+commit');
+  });
+
+  it('loadCompiler without the download', async () => {
+    const version = solc.version().split('+')[0];
+    const solcLoaded = await loadCompiler(
+      {compilerVersion: version} as Config
+    );
+    expect(solcLoaded).to.equal(solc);
+  });
+
   it('loadCompiler with solcVersion as path', async () => {
     const solcLoaded = await loadCompiler(
       {compilerVersion: '../node_modules/solc'} as Config
