a462187e16f166f1c1bd78fb06e9ab34470df3c4
Daniel Izdebski <spidiritsu@gmail.com>
2020-09-29 14:24:14 +0200

    Add isAccount utill function

--- waffle-chai/src/matchers/misc/account.ts
+++ waffle-chai/src/matchers/misc/account.ts
@@ -2,8 +2,12 @@ import {Contract, Signer, Wallet} from 'ethers';
 
 export type Account = Signer | Contract;
 
+export function isAccount(account: Account): account is Contract | Wallet {
+  return account instanceof Contract || account instanceof Wallet;
+}
+
 export async function getAddressOf(account: Account) {
-  if (account instanceof Contract || account instanceof Wallet) {
+  if (isAccount(account)) {
     return account.address;
   } else {
     return account.getAddress();
