7918903c715aeaa0fd5c1f59b89fc97ea645607e
Daniel Izdebski <spidiritsu@gmail.com>
2020-09-30 16:12:08 +0200

    Change tests so they are more correct

--- waffle-chai/test/matchers/changeBalance.test.ts
+++ waffle-chai/test/matchers/changeBalance.test.ts
@@ -23,7 +23,6 @@ describe('INTEGRATION: changeBalance matcher', () => {
         await expect(() =>
           sender.sendTransaction({
             to: receiver.address,
-            gasPrice: 0,
             value: 200
           })
         ).to.changeBalance(receiver, 200);
@@ -33,7 +32,6 @@ describe('INTEGRATION: changeBalance matcher', () => {
         await expect(() =>
           sender.sendTransaction({
             to: receiver.address,
-            gasPrice: 0,
             value: 200
           })
         ).to.changeBalance(receiver, BigNumber.from(200));
@@ -43,10 +41,19 @@ describe('INTEGRATION: changeBalance matcher', () => {
         await expect(() =>
           sender.sendTransaction({
             to: receiver.address,
-            gasPrice: 0,
             value: 200
           })
         ).to.not.changeBalance(receiver, BigNumber.from(300));
+      });
+
+      it('Should take into account transaction fee', async () => {
+        await expect(() =>
+            sender.sendTransaction({
+              to: receiver.address,
+              gasPrice: 1,
+              value: 200
+            })
+          ).to.changeBalance(sender, -21200)
       });
 
       it('Should throw when expected balance change value was different from an actual', async () => {
@@ -80,86 +87,11 @@ describe('INTEGRATION: changeBalance matcher', () => {
       });
     });
 
-    describe('Change balance, multiple accounts', () => {
-      it('Should pass when all expected balance changes are equal to actual values', async () => {
-        await expect(() =>
-          sender.sendTransaction({
-            to: receiver.address,
-            gasPrice: 0,
-            value: 200
-          })
-        ).to.changeBalances([sender, receiver], ['-200', 200]);
-      });
-
-      it('Should pass when negated and numbers don\'t match', async () => {
-        await expect(() =>
-          sender.sendTransaction({
-            to: receiver.address,
-            gasPrice: 0,
-            value: 200
-          })
-        ).to.not.changeBalances([sender, receiver], [-201, 200]);
-        await expect(() =>
-          sender.sendTransaction({
-            to: receiver.address,
-            gasPrice: 0,
-            value: 200
-          })
-        ).to.not.changeBalances([sender, receiver], [-200, 201]);
-      });
-
-      it('Should throw when expected balance change value was different from an actual for any wallet', async () => {
-        await expect(
-          expect(() =>
-            sender.sendTransaction({
-              to: receiver.address,
-              gasPrice: 0,
-              value: 200
-            })
-          ).to.changeBalances([sender, receiver], [-200, 201])
-        ).to.be.eventually.rejectedWith(
-          AssertionError,
-          'Expected 0x17ec8597ff92C3F44523bDc65BF0f1bE632917ff,0x63FC2aD3d021a4D7e64323529a55a9442C444dA0 ' +
-            'to change balance by -200,201 wei, but it has changed by -200,200 wei'
-        );
-        await expect(
-          expect(() =>
-            sender.sendTransaction({
-              to: receiver.address,
-              gasPrice: 0,
-              value: 200
-            })
-          ).to.changeBalances([sender, receiver], [-201, 200])
-        ).to.be.eventually.rejectedWith(
-          AssertionError,
-          'Expected 0x17ec8597ff92C3F44523bDc65BF0f1bE632917ff,0x63FC2aD3d021a4D7e64323529a55a9442C444dA0 ' +
-            'to change balance by -201,200 wei, but it has changed by -200,200 wei'
-        );
-      });
-
-      it('Should throw in negative case when expected balance changes value were equal to an actual', async () => {
-        await expect(
-          expect(() =>
-            sender.sendTransaction({
-              to: receiver.address,
-              gasPrice: 0,
-              value: 200
-            })
-          ).to.not.changeBalances([sender, receiver], [-200, 200])
-        ).to.be.eventually.rejectedWith(
-          AssertionError,
-          'Expected 0x17ec8597ff92C3F44523bDc65BF0f1bE632917ff,0x63FC2aD3d021a4D7e64323529a55a9442C444dA0 ' +
-            'to not change balance by -200,200 wei'
-        );
-      });
-    });
-
     describe('Change balance, one contract', () => {
       it('Should pass when expected balance change is passed as int and is equal to an actual', async () => {
         await expect(async () =>
           sender.sendTransaction({
             to: contract.address,
-            gasPrice: 0,
             value: 200
           })
         ).to.changeBalance(contract, 200);
@@ -169,15 +101,6 @@ describe('INTEGRATION: changeBalance matcher', () => {
 
   describe('Transaction Response', () => {
     describe('Change balance, one account', () => {
-      it('await send transaction', async () => {
-        await expect(await sender.sendTransaction({
-          to: receiver.address,
-          gasPrice: 0,
-          value: 200
-        })
-        ).to.changeBalance(sender, -200);
-      });
-
       it('Should pass when expected balance change is passed as string and is equal to an actual', async () => {
         await expect(await sender.sendTransaction({
           to: receiver.address,
@@ -208,7 +131,6 @@ describe('INTEGRATION: changeBalance matcher', () => {
       it('Should pass on negative case when expected balance change is not equal to an actual', async () => {
         await expect(await sender.sendTransaction({
           to: receiver.address,
-          gasPrice: 0,
           value: 200
         })
         ).to.not.changeBalance(receiver, BigNumber.from(300));
@@ -239,74 +161,6 @@ describe('INTEGRATION: changeBalance matcher', () => {
         ).to.be.eventually.rejectedWith(
           AssertionError,
           `Expected "${sender.address}" to not change balance by -200 wei`
-        );
-      });
-    });
-
-    describe('Change balance, multiple accounts', () => {
-      it('Should pass when all expected balance changes are equal to actual values', async () => {
-        await expect(await sender.sendTransaction({
-          to: receiver.address,
-          gasPrice: 0,
-          value: 200
-        })
-        ).to.changeBalances([sender, receiver], ['-200', 200]);
-      });
-
-      it('Should pass when negated and numbers don\'t match', async () => {
-        await expect(await sender.sendTransaction({
-          to: receiver.address,
-          gasPrice: 0,
-          value: 200
-        })
-        ).to.not.changeBalances([sender, receiver], [-201, 200]);
-        await expect(await sender.sendTransaction({
-          to: receiver.address,
-          gasPrice: 0,
-          value: 200
-        })
-        ).to.not.changeBalances([sender, receiver], [-200, 201]);
-      });
-
-      it('Should throw when expected balance change value was different from an actual for any wallet', async () => {
-        await expect(
-          expect(await sender.sendTransaction({
-            to: receiver.address,
-            gasPrice: 0,
-            value: 200
-          })
-          ).to.changeBalances([sender, receiver], [-200, 201])
-        ).to.be.eventually.rejectedWith(
-          AssertionError,
-          'Expected 0x17ec8597ff92C3F44523bDc65BF0f1bE632917ff,0x63FC2aD3d021a4D7e64323529a55a9442C444dA0 ' +
-            'to change balance by -200,201 wei, but it has changed by -200,200 wei'
-        );
-        await expect(
-          expect(await sender.sendTransaction({
-            to: receiver.address,
-            gasPrice: 0,
-            value: 200
-          })
-          ).to.changeBalances([sender, receiver], [-201, 200])
-        ).to.be.eventually.rejectedWith(
-          AssertionError,
-          'Expected 0x17ec8597ff92C3F44523bDc65BF0f1bE632917ff,0x63FC2aD3d021a4D7e64323529a55a9442C444dA0 ' +
-            'to change balance by -201,200 wei, but it has changed by -200,200 wei'
-        );
-      });
-
-      it('Should throw in negative case when expected balance changes value were equal to an actual', async () => {
-        await expect(
-          expect(await sender.sendTransaction({
-            to: receiver.address,
-            gasPrice: 0,
-            value: 200
-          })
-          ).to.not.changeBalances([sender, receiver], [-200, 200])
-        ).to.be.eventually.rejectedWith(
-          AssertionError,
-          'Expected 0x17ec8597ff92C3F44523bDc65BF0f1bE632917ff,0x63FC2aD3d021a4D7e64323529a55a9442C444dA0 ' +
-            'to not change balance by -200,200 wei'
         );
       });
     });
--- waffle-chai/test/matchers/changeBalances.test.ts
+++ waffle-chai/test/matchers/changeBalances.test.ts
@@ -1,4 +1,4 @@
-import {expect} from 'chai';
+import {AssertionError, expect} from 'chai';
 import {MockProvider} from '@ethereum-waffle/provider';
 import {Contract} from 'ethers';
 
@@ -19,6 +19,80 @@ describe('INTEGRATION: changeBalances matcher', () => {
         ).to.changeBalances([sender, contract], [-200, 200]);
       });
     });
+
+    describe('Change balance, multiple accounts', () => {
+      it('Should pass when all expected balance changes are equal to actual values', async () => {
+        await expect(() =>
+          sender.sendTransaction({
+            to: receiver.address,
+            gasPrice: 0,
+            value: 200
+          })
+        ).to.changeBalances([sender, receiver], ['-200', 200]);
+      });
+
+      it('Should pass when negated and numbers don\'t match', async () => {
+        await expect(() =>
+          sender.sendTransaction({
+            to: receiver.address,
+            gasPrice: 0,
+            value: 200
+          })
+        ).to.not.changeBalances([sender, receiver], [-201, 200]);
+        await expect(() =>
+          sender.sendTransaction({
+            to: receiver.address,
+            gasPrice: 0,
+            value: 200
+          })
+        ).to.not.changeBalances([sender, receiver], [-200, 201]);
+      });
+
+      it('Should throw when expected balance change value was different from an actual for any wallet', async () => {
+        await expect(
+          expect(() =>
+            sender.sendTransaction({
+              to: receiver.address,
+              gasPrice: 0,
+              value: 200
+            })
+          ).to.changeBalances([sender, receiver], [-200, 201])
+        ).to.be.eventually.rejectedWith(
+          AssertionError,
+          'Expected 0x17ec8597ff92C3F44523bDc65BF0f1bE632917ff,0x63FC2aD3d021a4D7e64323529a55a9442C444dA0 ' +
+            'to change balance by -200,201 wei, but it has changed by -200,200 wei'
+        );
+        await expect(
+          expect(() =>
+            sender.sendTransaction({
+              to: receiver.address,
+              gasPrice: 0,
+              value: 200
+            })
+          ).to.changeBalances([sender, receiver], [-201, 200])
+        ).to.be.eventually.rejectedWith(
+          AssertionError,
+          'Expected 0x17ec8597ff92C3F44523bDc65BF0f1bE632917ff,0x63FC2aD3d021a4D7e64323529a55a9442C444dA0 ' +
+            'to change balance by -201,200 wei, but it has changed by -200,200 wei'
+        );
+      });
+
+      it('Should throw in negative case when expected balance changes value were equal to an actual', async () => {
+        await expect(
+          expect(() =>
+            sender.sendTransaction({
+              to: receiver.address,
+              gasPrice: 0,
+              value: 200
+            })
+          ).to.not.changeBalances([sender, receiver], [-200, 200])
+        ).to.be.eventually.rejectedWith(
+          AssertionError,
+          'Expected 0x17ec8597ff92C3F44523bDc65BF0f1bE632917ff,0x63FC2aD3d021a4D7e64323529a55a9442C444dA0 ' +
+            'to not change balance by -200,200 wei'
+        );
+      });
+    });
   });
 
   describe('Transaction Response', () => {
@@ -30,6 +104,74 @@ describe('INTEGRATION: changeBalances matcher', () => {
           value: 200
         })
         ).to.changeBalances([sender, contract], [-200, 200]);
+      });
+    });
+
+    describe('Change balance, multiple accounts', () => {
+      it('Should pass when all expected balance changes are equal to actual values', async () => {
+        await expect(await sender.sendTransaction({
+          to: receiver.address,
+          gasPrice: 0,
+          value: 200
+        })
+        ).to.changeBalances([sender, receiver], ['-200', 200]);
+      });
+
+      it('Should pass when negated and numbers don\'t match', async () => {
+        await expect(await sender.sendTransaction({
+          to: receiver.address,
+          gasPrice: 0,
+          value: 200
+        })
+        ).to.not.changeBalances([sender, receiver], [-201, 200]);
+        await expect(await sender.sendTransaction({
+          to: receiver.address,
+          gasPrice: 0,
+          value: 200
+        })
+        ).to.not.changeBalances([sender, receiver], [-200, 201]);
+      });
+
+      it('Should throw when expected balance change value was different from an actual for any wallet', async () => {
+        await expect(
+          expect(await sender.sendTransaction({
+            to: receiver.address,
+            gasPrice: 0,
+            value: 200
+          })
+          ).to.changeBalances([sender, receiver], [-200, 201])
+        ).to.be.eventually.rejectedWith(
+          AssertionError,
+          'Expected 0x17ec8597ff92C3F44523bDc65BF0f1bE632917ff,0x63FC2aD3d021a4D7e64323529a55a9442C444dA0 ' +
+            'to change balance by -200,201 wei, but it has changed by -200,200 wei'
+        );
+        await expect(
+          expect(await sender.sendTransaction({
+            to: receiver.address,
+            gasPrice: 0,
+            value: 200
+          })
+          ).to.changeBalances([sender, receiver], [-201, 200])
+        ).to.be.eventually.rejectedWith(
+          AssertionError,
+          'Expected 0x17ec8597ff92C3F44523bDc65BF0f1bE632917ff,0x63FC2aD3d021a4D7e64323529a55a9442C444dA0 ' +
+            'to change balance by -201,200 wei, but it has changed by -200,200 wei'
+        );
+      });
+
+      it('Should throw in negative case when expected balance changes value were equal to an actual', async () => {
+        await expect(
+          expect(await sender.sendTransaction({
+            to: receiver.address,
+            gasPrice: 0,
+            value: 200
+          })
+          ).to.not.changeBalances([sender, receiver], [-200, 200])
+        ).to.be.eventually.rejectedWith(
+          AssertionError,
+          'Expected 0x17ec8597ff92C3F44523bDc65BF0f1bE632917ff,0x63FC2aD3d021a4D7e64323529a55a9442C444dA0 ' +
+            'to not change balance by -200,200 wei'
+        );
       });
     });
   });
