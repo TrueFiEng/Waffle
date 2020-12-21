9a921bd6749dd5cac747d2a7ab3552691cc61f5f
Benjamin <benjamin.mateo@protonmail.com>
2020-12-21 19:48:14 +0100

    feat(event): add assertion for indexed dynamic size parameters (#413)
    * feat(event): add assertion for indexed dynamic size parameters
    
    * fix(emit): accept hash and non hash values
    
    Co-authored-by: Marek Kirejczyk <marekkirejczyk@users.noreply.github.com>

--- waffle-chai/src/matchers/emit.ts
+++ waffle-chai/src/matchers/emit.ts
@@ -66,7 +66,15 @@ export function supportEmit(Assertion: Chai.AssertionStatic) {
           new Assertion(actualArgs[index][j]).equal(expectedArgs[index][j]);
         }
       } else {
-        new Assertion((actualArgs[index])).equal((expectedArgs[index]));
+        if (actualArgs[index].hash !== undefined && actualArgs[index]._isIndexed === true) {
+          const expectedArgBytes = utils.isHexString(expectedArgs[index])
+            ? utils.arrayify(expectedArgs[index]) : utils.toUtf8Bytes(expectedArgs[index]);
+          new Assertion(actualArgs[index].hash).to.be.oneOf(
+            [expectedArgs[index], utils.keccak256(expectedArgBytes)]
+          );
+        } else {
+          new Assertion(actualArgs[index]).equal(expectedArgs[index]);
+        }
       }
     }
   };
