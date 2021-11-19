5f7b150500766f29b772b1aed535f868838f3fb1
Ivan <ivanruch@gmail.com>
2021-11-19 12:51:23 +0100

    fix lint

--- waffle-geth/src/native.ts
+++ waffle-geth/src/native.ts
@@ -1,15 +1,15 @@
 import ffi from 'ffi-napi';
-import { join } from 'path'
+import {join} from 'path';
 
 const library = ffi.Library(join(__dirname, '../go/build/wafflegeth.dylib'), {
   cgoCurrentMillis: ['int', []],
-  getBlockNumber: ['string', []],
-})
+  getBlockNumber: ['string', []]
+});
 
 export function cgoCurrentMillis() {
-  return library.cgoCurrentMillis()
+  return library.cgoCurrentMillis();
 }
 
 export function getBlockNumber() {
-  return library.getBlockNumber()
+  return library.getBlockNumber();
 }
--- waffle-geth/test/native.test.ts
+++ waffle-geth/test/native.test.ts
@@ -1,13 +1,13 @@
-import { expect } from 'chai';
+import {expect} from 'chai';
 import {cgoCurrentMillis, getBlockNumber} from '../src/native';
 
 describe('Native', () => {
-    it('can call a native function', () => {
-        expect(cgoCurrentMillis()).to.be.a('number')
-        expect(cgoCurrentMillis()).to.be.gt(0)
-    })
+  it('can call a native function', () => {
+    expect(cgoCurrentMillis()).to.be.a('number');
+    expect(cgoCurrentMillis()).to.be.gt(0);
+  });
 
   it('can get block number', () => {
-    expect(getBlockNumber()).to.equal('0')
-  })
-})
+    expect(getBlockNumber()).to.equal('0');
+  });
+});
