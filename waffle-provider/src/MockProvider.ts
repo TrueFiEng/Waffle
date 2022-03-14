087894933be2947117f23c10eaeb85ebcc5dc9b4
rzadp <roopert7@gmail.com>
2022-03-14 12:36:25 +0100

    Use Ganache instead of ganache-core

--- waffle-provider/src/MockProvider.ts
+++ waffle-provider/src/MockProvider.ts
@@ -1,13 +1,14 @@
 import {providers, Wallet} from 'ethers';
 import {CallHistory, RecordedCall} from './CallHistory';
 import {defaultAccounts} from './defaultAccounts';
-import type Ganache from 'ganache-core';
+import {EthereumProviderOptions} from 'ganache';
+
 import {deployENS, ENS} from '@ethereum-waffle/ens';
 
 export {RecordedCall};
 
 interface MockProviderOptions {
-  ganacheOptions: EthereumProviderOptions
+  ganacheOptions: EthereumProviderOptions;
 }
 
 export class MockProvider extends providers.Web3Provider {
@@ -15,13 +16,13 @@ export class MockProvider extends providers.Web3Provider {
   private _ens?: ENS;
 
   constructor(private options?: MockProviderOptions) {
-    super(require('ganache-core').provider({accounts: defaultAccounts, ...options?.ganacheOptions}) as any);
+    super(require('ganache').provider({accounts: defaultAccounts, ...options?.ganacheOptions}) as any);
     this._callHistory = new CallHistory();
     this._callHistory.record(this);
   }
 
   getWallets() {
-    const items = this.options?.ganacheOptions.accounts ?? defaultAccounts;
+    const items = this.options?.ganacheOptions.wallet?.accounts ?? defaultAccounts;
     return items.map((x: any) => new Wallet(x.secretKey, this));
   }
 
