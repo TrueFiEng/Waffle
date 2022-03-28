e4e3fe41bd9e6a37476cabf98bcfb668a4a29b1b
Przemyslaw Rzad <roopert7@gmail.com>
2022-03-28 11:46:58 +0200

    Update waffle-chai/src/matchers/revertedWith.ts

--- waffle-chai/src/matchers/revertedWith.ts
+++ waffle-chai/src/matchers/revertedWith.ts
@@ -29,7 +29,7 @@ export function supportRevertedWith(Assertion: Chai.AssertionStatic) {
           isReverted,
           `Expected transaction to be reverted with "${revertReason}", but other reason was found: "${revertString}"`,
           `Expected transaction NOT to be reverted with "${revertReason}"`,
-          `Transaction reverted with "${revertReason}."`,
+          `Transaction reverted with "${revertReason}"`,
           error
         );
         return error;
