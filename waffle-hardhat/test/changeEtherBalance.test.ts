0e1ea0445842d0e36634ac1e4c36352625baca38
yivlad <jacenko.vlad@gmail.com>
2022-04-06 16:11:40 +0200

    Remove chai from waffle-hardhat

--- waffle-hardhat/test/changeEtherBalance.test.ts
+++ waffle-hardhat/test/changeEtherBalance.test.ts
@@ -1,10 +1,6 @@
-import chai from 'chai';
 import {waffle} from 'hardhat';
 import {MockProvider} from 'ethereum-waffle';
-import chaiAsPromised from 'chai-as-promised';
 import {changeEtherBalanceTest} from '@ethereum-waffle/chai/test';
-
-chai.use(chaiAsPromised);
 
 describe('INTEGRATION: changeEtherBalance matcher', () => {
   const provider = waffle.provider as MockProvider;
