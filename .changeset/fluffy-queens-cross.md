aa18bfae1ce1bc720a3b430b745d3a221cdfd0e5
Vladyslav Yatsenko <52505649+yivlad@users.noreply.github.com>
2022-04-19 14:50:50 +0200

    🌭 Improve hardhat error catching (#707)

---
+++ .changeset/fluffy-queens-cross.md
@@ -0,0 +1,5 @@
+---
+"@ethereum-waffle/chai": patch
+---
+
+Improve hardhat error catching
--- waffle-hardhat/test/reverted.test.ts
+++ waffle-hardhat/test/reverted.test.ts
@@ -1,6 +1,9 @@
 import {waffle} from 'hardhat';
+import {expect} from 'chai';
 import {MockProvider} from 'ethereum-waffle';
 import {revertedTest, revertedWithTest} from '@ethereum-waffle/chai/test';
+import {ContractFactory} from 'ethers';
+import {abi, bytecode} from '../build/contracts/Panic.sol/Panic.json';
 
 describe('INTEGRATION: Matchers: reverted', () => {
   const provider = waffle.provider as MockProvider;
@@ -20,4 +23,13 @@ describe('INTEGRATION: Matchers: revertedWith', () => {
   });
 
   revertedWithTest(provider);
+});
+
+it('Panic code', async () => {
+  await waffle.provider.send('hardhat_reset', []);
+  const wallets = waffle.provider.getWallets();
+  const wallet = wallets[0];
+  const factory = new ContractFactory(abi, bytecode, wallet);
+  const panicContract = await factory.deploy();
+  await expect(panicContract.panic()).to.be.revertedWith('panic code 0x32');
 });
