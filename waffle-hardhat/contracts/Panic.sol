3b4ae05cf4b631f7652a6d2162ff909a3508b7e6
yivlad <jacenko.vlad@gmail.com>
2022-04-19 14:19:01 +0200

    Add panic test

---
+++ waffle-hardhat/contracts/Panic.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.10;
+
+contract Panic {
+  uint[] private arr;
+  function panic() public {
+      uint a = arr[254];
+  }
+}
--- waffle-hardhat/hardhat.config.ts
+++ waffle-hardhat/hardhat.config.ts
@@ -6,5 +6,22 @@ module.exports = {
       initialDate: '2020-01-01T00:00:00',
       allowUnlimitedContractSize: true,
     },
-  }
+  },
+  paths: {
+    sources: './contracts',
+    artifacts: './build',
+    cache: './cache',
+  },
+  abiExporter: {
+    path: './build',
+    flat: true,
+    spacing: 2,
+  },
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.10"
+      }
+    ]
+  } 
 }
