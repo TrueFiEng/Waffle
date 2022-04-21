8d4934c23cdf2c95df7248d9031e9135fbb2e33e
yivlad <jacenko.vlad@gmail.com>
2022-04-21 14:17:06 +0200

    Test again

--- scripts/dev-version.sh
+++ scripts/dev-version.sh
@@ -3,3 +3,4 @@
 set -e
 DEV=$(jq -r ".version" package.json | awk -F "-" '{print $1}')-dev.$(git rev-parse --short HEAD)
 cat <<< "$(jq --arg DEV "$DEV" ".version = \"$DEV\"" package.json)" > package.json
+cat <<< "$(jq ".publishConfig.tag = \"dev\"" package.json)" > package.json
