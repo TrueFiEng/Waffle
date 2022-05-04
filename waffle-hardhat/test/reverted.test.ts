ca5a34612cc8ca75a3fe618b628f1e21eac75dbf
yivlad <jacenko.vlad@gmail.com>
2022-05-04 16:06:42 +0200

    Hardhat support custom errors

--- waffle-hardhat/test/reverted.test.ts
+++ waffle-hardhat/test/reverted.test.ts
@@ -3,7 +3,8 @@ import {expect} from 'chai';
 import {MockProvider} from 'ethereum-waffle';
 import {revertedTest, revertedWithTest} from '@ethereum-waffle/chai/test';
 import {ContractFactory} from 'ethers';
-import {abi, bytecode} from '../build/contracts/Panic.sol/Panic.json';
+import Panic from '../build/contracts/Panic.sol/Panic.json';
+import CustomError from '../build/contracts/CustomError.sol/Matchers.json'
 
 describe('INTEGRATION: Matchers: reverted', () => {
   const provider = waffle.provider as MockProvider;
@@ -29,7 +30,16 @@ it('Panic code', async () => {
   await waffle.provider.send('hardhat_reset', []);
   const wallets = waffle.provider.getWallets();
   const wallet = wallets[0];
-  const factory = new ContractFactory(abi, bytecode, wallet);
+  const factory = new ContractFactory(Panic.abi, Panic.bytecode, wallet);
   const panicContract = await factory.deploy();
   await expect(panicContract.panic()).to.be.revertedWith('panic code 0x32');
+});
+
+it('Handle custom error', async () => {
+  await waffle.provider.send('hardhat_reset', []);
+  const wallets = waffle.provider.getWallets();
+  const wallet = wallets[0];
+  const factory = new ContractFactory(CustomError.abi, CustomError.bytecode, wallet);
+  const matchers = await factory.deploy();
+  await expect(matchers.doRevertWithCustomError()).to.be.revertedWith('CustomError(0)');
 });
