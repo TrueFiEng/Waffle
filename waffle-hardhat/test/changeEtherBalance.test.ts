bbd0c19268e01646df8e51adf938f97900c131c6
FIRST_NAME LAST_NAME <jacenko.vlad@gmail.com>
2022-05-13 12:04:11 +0200

    Fix hardhat tests

--- waffle-hardhat/test/changeEtherBalance.test.ts
+++ waffle-hardhat/test/changeEtherBalance.test.ts
@@ -1,6 +1,7 @@
 import {waffle} from 'hardhat';
 import {MockProvider} from 'ethereum-waffle';
 import {changeEtherBalanceTest} from '@ethereum-waffle/chai/test';
+import {TX_GAS, BASE_FEE_PER_GAS} from './constants';
 
 describe('INTEGRATION: changeEtherBalance matcher', () => {
   const provider = waffle.provider as MockProvider;
@@ -9,5 +10,5 @@ describe('INTEGRATION: changeEtherBalance matcher', () => {
     await provider.send('hardhat_reset', []);
   });
 
-  changeEtherBalanceTest(provider);
+  changeEtherBalanceTest(provider, {txGasFees: TX_GAS * BASE_FEE_PER_GAS, baseFeePerGas: BASE_FEE_PER_GAS});
 });
