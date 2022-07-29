4ba80cfff3bf80e82651dbf7be0403359afda136
rzadp <roopert7@gmail.com>
2022-07-29 15:30:07 +0200

    👩🏻‍🍳 MockProvider: Use actual wallets from ganache in `getWallets`

---
+++ waffle-provider/test/MockProviderWallets.test.ts
@@ -0,0 +1,66 @@
+import { expect } from 'chai';
+import { utils, Wallet } from 'ethers';
+import { MockProvider } from '../src/MockProvider';
+
+describe('MockProvider - Ganache Wallets', async () => {
+  const assertWalletsWithBalances = async (provider: MockProvider, wallets: Wallet[]) => {
+    for (const wallet of wallets) {
+      const balance = await wallet.getBalance();
+      expect(balance.gt(0)).to.equal(true);
+      expect(wallet.provider).to.equal(provider);
+    }
+  };
+
+  it('returns default wallets', async () => {
+    const provider = new MockProvider();
+    const wallets = provider.getWallets();
+    expect(wallets.length).to.equal(10);
+    await assertWalletsWithBalances(provider, wallets);
+  });
+
+  it('Can generate a different number of accounts', async () => {
+    const provider = new MockProvider({
+      ganacheOptions: {
+        wallet: {
+          totalAccounts: 25
+        }
+      }
+    });
+    const wallets = provider.getWallets();
+    expect(wallets.length).to.equal(25);
+    await assertWalletsWithBalances(provider, wallets);
+  });
+
+  it('Can generate accounts based on a seed', async () => {
+    const mnemonic = Wallet.createRandom().mnemonic;
+    const provider = new MockProvider({
+      ganacheOptions: {
+        wallet: {
+          totalAccounts: 25,
+          mnemonic: mnemonic.phrase
+        }
+      }
+    });
+    const wallets = provider.getWallets();
+    expect(wallets.length).to.equal(25);
+    await assertWalletsWithBalances(provider, wallets);
+
+    const defaultProvider = new MockProvider();
+    expect(defaultProvider.getWallets()[0].address).to.not.be.eq(wallets[0].address);
+  });
+
+  it('Can generate wallets with non-default balance', async () => {
+    const provider = new MockProvider({
+      ganacheOptions: {
+        wallet: {
+          totalAccounts: 25,
+          defaultBalance: 101
+        }
+      }
+    });
+    const wallets = provider.getWallets();
+    expect(wallets.length).to.equal(25);
+    await assertWalletsWithBalances(provider, wallets);
+    expect((await wallets[0].getBalance()).toString()).to.eq(utils.parseEther('101').toString());
+  });
+});
